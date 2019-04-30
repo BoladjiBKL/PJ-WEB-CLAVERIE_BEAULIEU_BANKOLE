1b231f62a93a05106b1d05d45d6d0d0959cb37fd
MathieuClv <claveriemathieu@hotmail.fr>
2019-04-30 18:37:57 +0200

    resolutionpb3

--- ajouter_musique.php
+++ ajouter_musique.php
@@ -1,5 +1,4 @@
 
-
 <?php
 //recuperer les données venant de la page HTML
 //le parametre de $_POST = "name" de <input> de votre page HTML
@@ -7,6 +6,7 @@ $titre = isset($_POST["titre"])? $_POST["titre"] : "";
 $artiste = isset($_POST["artiste"])? $_POST["artiste"] : "";
 $annee = isset($_POST["annee"])? $_POST["annee"] : "";
 $label = isset($_POST["label"])? $_POST["label"] : "";
+$description = isset($_POST["description"])? $_POST["description"] : "";
 $prix = isset($_POST["prix"])? $_POST["prix"] : "";
 $mail = isset($_POST["mail"])? $_POST["mail"] : "";
 
@@ -15,12 +15,13 @@ $bdd = new PDO('mysql:host=localhost;dbname=ECEAmazon;charset=utf8', 'root', 'ro
 
 
 
-$req = $bdd->prepare('INSERT INTO musique(titre, artiste, annee, label, prix, mail) VALUES(:titre, :artiste, :annee, :label, :prix, :mail)');
+$req = $bdd->prepare('INSERT INTO musique(titre, artiste, annee, label, description, prix, mail) VALUES(:titre, :artiste, :annee, :label, :description, :prix, :mail)');
 $req->execute(array(
 	'titre' => $titre,
 	'artiste' => $artiste,
 	'annee' => $annee,
 	'label' => $label,
+	'description' => $description,
 	'prix' => $prix,
 	'mail' => $mail,
 	));
