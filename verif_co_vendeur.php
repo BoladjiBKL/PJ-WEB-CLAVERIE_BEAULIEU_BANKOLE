a53475925643ba2d21a52edf3c2b2f73773cde52
BoladjiBKL <whbbkl@gmail.com>
2019-05-02 01:10:17 +0200

    Update verif_co_vendeur.php

--- verif_co_vendeur.php
+++ verif_co_vendeur.php
@@ -2,10 +2,19 @@
 
 $mailvend = isset($_POST["mailvend"])? $_POST["mailvend"] : "";
 $pseudo= isset($_POST["pseudo"])? $_POST["pseudo"] : "";
-
+$nom= isset($_POST["nom"])? $_POST["nom"] : "";
 
 $error  ="";
 $drapeau =0;
+
+$bdd = new PDO('mysql:host=localhost;dbname=ECEAmazon;charset=utf8', 'root', 'root');
+$verf= $bdd->prepare('SELECT * FROM vendeur WHERE mailvend= :mailvend AND pseudo= :pseudo');
+$verf->execute(array(
+	'mailvend' => $mailvend,
+	'pseudo' => $pseudo,
+));
+
+$donnees= $verf->fetch();
 
 
 if ($mailvend=="") {
@@ -22,16 +31,17 @@ echo($error);
 
 
 
-if ($error!="") 
+if ($error="" && $donnees) 
 {   
+	 header('Location: Compte_vendeur.php');
 	
-	header('Location: Co_vendeur.php');
 
 }
  else
  {
-  header('Location: Compte_vendeur.php');
+  header('Location: Co_vendeur.php');
  }
+ 
 
 
 
