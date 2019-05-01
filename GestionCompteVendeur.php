3a7e46bbb72a4551cf8dfaee24ede5120513df63
BoladjiBKL <whbbkl@gmail.com>
2019-05-01 17:21:27 +0200

    Update GestionCompteVendeur.php

--- GestionCompteVendeur.php
+++ GestionCompteVendeur.php
@@ -7,39 +7,44 @@ $Action=isset($_POST["Action"])?$_POST["Action"]:"";
 
 
 
-   if ($Type="Livres" && $Action="Ajouter") 
+   if ($Type=="Livres" && $Action=="Ajouter") 
  	{
     header('Location: formulaire_ajout_livre.php');
     }
 
     
-   if ($Type="Livres" && $Action="Supprimer") 
+   if ($Type=="Livres" && $Action=="Supprimer") 
     {
     header('Location: formulaire_supprimer_livre.php');
     }   
 
-    if ($Type="Vetements" && $Action="Ajouter") 
+
+    if ($Type=="Vetements" && $Action=="Ajouter") 
     {
     header('Location: formulaire_ajouter_vetements.php');
     }
-    if ($Type="Vetements" && $Action="Supprimer") 
+
+    if ($Type=="Vetements" && $Action=="Supprimer") 
     {
     header('Location: formulaire_supprimer_vetements.php');
     }
-    if ($Type="Sport et Loisir" && $Action="Ajouter") 
+
+    if ($Type=="Sport et Loisir" && $Action=="Ajouter") 
     {
     header('Location: formulaire_ajouter_sportetloisir.php');
     }
-    if ($Type="Sport et Loisir" && $Action="Supprimer") 
+
+    if ($Type=="Sport et Loisir" && $Action=="Supprimer") 
     {
     header('Location: formulaire_supprimer_sportetloisir.php');
     }
-    if ($Type="Musique" && $Action="Ajouter") 
+
+    if ($Type=="Musique" && $Action=="Ajouter") 
     {
     header('Location: formulaire_ajout_musique.php');
     }
     
-    if ($Type="Musique" && $Action="Supprimer") 
+    if ($Type=="Musique" && $Action=="Supprimer") 
     {
     header('Location: formulaire_supprimer_musique.php');
     }
