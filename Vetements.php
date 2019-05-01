561b0dab5c57e64010863dad32790dc10a6052bb
Paul Beaulieu <beaulieu1paul@gmail.com>
2019-05-01 18:40:06 +0200

    Update Vetements.php

--- Vetements.php
+++ Vetements.php
@@ -1,16 +1,4 @@
 
-<?php
-
-$bdd = new PDO('mysql:host=localhost;dbname=ECEAmazon;charset=utf8', 'root', 'root');
-
-$reponse = $bdd->query('SELECT * FROM vetement');
-
-// On affiche chaque entrée une à une
-while ($donnees = $reponse->fetch())
-{
-?>
-
-
 
 
 
@@ -29,11 +17,13 @@ while ($donnees = $reponse->fetch())
     <link rel="stylesheet" type="text/css" href="tous.css">
     <link rel="stylesheet" type="text/css" href="Vetements.css">
 </head>
+
+
 <body>
     <!-- barre de navigation -->
-       <nav class="navbar navbar navbar-expand-lg navbar-lightgreen bg-lightgreen">
+    <nav class="navbar navbar navbar-expand-lg navbar-lightgreen bg-lightgreen">
 
-  <a class="navbar-brand" href="Accueil.html"><img src="eceamazon.png" height="60px"></a>
+  <a class="navbar-brand" href="Accueil.php"><img src="eceamazon.png" height="60px"></a>
 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -45,32 +35,32 @@ while ($donnees = $reponse->fetch())
     <ul class="navbar-nav">
       <li class="nav-item active">
         
-
+        
         <div class="dropdown">
-          <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <a class="btn btn-secondary dropdown-toggle"  role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Catégories
           </a>
 
             <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <a class="dropdown-item" href="Livres.html" >Livres</a>
-                <a class="dropdown-item" href="Vetements.html">Vétements</a>
-                <a class="dropdown-item" href="Sport&Loisir.html">Sports et Loisir</a>
-                <a class="dropdown-item" href="Musique.html">Musique</a>
+                <a class="dropdown-item" href="Livres.php" >Livres</a>
+                <a class="dropdown-item" href="Vetements.php">Vétements</a>
+                <a class="dropdown-item" href="Sport&Loisir.php">Sports et Loisir</a>
+                <a class="dropdown-item" href="Musique.php">Musique</a>
                </div>
         </div>
       </li>
 
       <li class="nav-item">
-        <a class="nav-link" href="Ventes_Flash.html">Ventes Flash</a>
+        <a class="nav-link" href="Ventes_Flash.php">Ventes Flash</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="Co_vendeur.html">Vendre</a>
+        <a class="nav-link" href="Co_vendeur.php">Vendre</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="Co_mon_compte.html">Mon Compte</a>
+        <a class="nav-link" href="Co_mon_compte.php">Mon Compte</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="Co_admin.html">Admin</a>
+        <a class="nav-link" href="Co_admin.php">Admin</a>
       </li>
      
 
@@ -78,39 +68,70 @@ while ($donnees = $reponse->fetch())
     </ul>
   </div>
   
-  <a class="nav-link" href="Panier.html"><img src="panier.png"></a>
+  <a class="nav-link" href="Panier.php"><img src="panier.png"></a>
+
 </nav>
 
 <br>
 
 <br>
+
 <div id="global">
-
-
-<p>Vetement a éditer</p>
-
- <div class="objet1">   
-   <strong>Nom</strong> : <?php echo $donnees['nom']; ?><br />
-    Taille : <?php echo $donnees['taille']; ?><br />
-    Description : <em><?php echo $donnees['description']; ?></em> <br />
-    <img src="<?php echo $donnees['urlimg'];?>" /><br />
-    Prix : <?php echo $donnees['prix']; ?><br />
-    Mail du vendeur : <?php echo $donnees['mail']; ?><br />
-
-   </div>
-
-
-
-
-
+  <p>Vetement a éditer</p>
 </div>
+
+
+
+
+
+  <?php
+
+  $bdd = new PDO('mysql:host=localhost;dbname=ECEAmazon;charset=utf8', 'root', 'root');
+
+  $reponse = $bdd->query('SELECT * FROM vetement');
+
+  ?>
+
+  <?php  
+     // On affiche chaque entrée une à une
+  while ($donnees = $reponse->fetch())
+  {
+    ?>
+
+     <div class="objet">
+
+      <strong>Nom</strong> : <?php echo $donnees['nom']; ?><br /><br />
+
+
+      <img class='image' src="<?php echo $donnees['urlimg'];?>" /> <br><br>
+
+      Taille : <?php echo $donnees['taille']; ?><br />
+
+      Description : <em><?php echo $donnees['description']; ?></em> <br />
+
+      Prix : <?php echo $donnees['prix']; ?> &euro;<br />
+
+      Mail du vendeur : <?php echo $donnees['mail']; ?><br />
+
+      Ajouter au panier
+      <br><br><br><br><br>
+      
+
+      </div>
+
+
+
+
+
+
+
 
 
  <footer class="footer">
 
     <!-- Copyright -->
     <div class="footer-copyright text-center py-3">© 2019 Copyright:
-      <a href="Accueil.html"> ECE-Amazon.com</a>
+      <a href="Accueil.php"> ECE-Amazon.com</a>
     </div>
     <!-- Copyright -->
 
@@ -126,7 +147,7 @@ while ($donnees = $reponse->fetch())
 
 
 
-   
+  
 
 
 
