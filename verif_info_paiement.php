f5386f46c697bc63c9b3b8609ccacd2ddcfc85c7
MathieuClv <claveriemathieu@hotmail.fr>
2019-05-03 15:26:15 +0200

    Update verif_info_paiement.php

--- verif_info_paiement.php
+++ verif_info_paiement.php
@@ -7,19 +7,14 @@ $nomcarte= isset($_POST["nomcarte"])? $_POST["nomcarte"] : "";
 $datecarte= isset($_POST["datecarte"])? $_POST["datecarte"] : "";
 $codecarte= isset($_POST["codecarte"])? $_POST["codecarte"] : "";
 
-
 $bdd = new PDO('mysql:host=localhost;dbname=ECEAmazon;charset=utf8', 'root', 'root');
 
 $bdd->exec("INSERT INTO bestsellers SELECT * FROM panier");
 
 $bdd->exec("DELETE FROM panier ");
 
-echo "bonjour";
-
 session_start();
-
 unset($_SESSION["newsession"]);
 
 
 ?>
-
