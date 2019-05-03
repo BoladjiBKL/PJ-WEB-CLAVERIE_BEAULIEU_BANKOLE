7bfeb0c4fe80d0fb5aa200f11397c74ffa580be2
Paul Beaulieu <beaulieu1paul@gmail.com>
2019-05-03 11:20:48 +0200

    header + footer

--- formulaire_supprimer_musique.php
+++ formulaire_supprimer_musique.php
@@ -1,34 +1,130 @@
+
 <!DOCTYPE html>
 <html>
-	<head>
-			<title>The Music Adder</title>
-			<meta charset="utf-8">
-	</head>
-	<body>
-			<h2>Le supprimeur de musiques</h2>
-			<form action="Supprimer_musique.php" method="post">
-					<table>
-							<tr>
-								<td>Titre:</td>
-								<td><input type="text" name="titre"></td>
-							</tr>
-							<tr>
-								<td>Artiste:</td>
-								<td><input type="text" name="artiste"></td>
-							</tr>
-							<tr>
-								<td>Mail:</td>
-								<td><input type="text" name="mail"></td>
-							</tr>
-								<tr>
-								<td colspan="2" align="center">
- 								<input type="submit" name="button2" value="Supprimer"></td>
+<head>
+  <title>Supprimer  musiques</title>
+  <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-								<td>
-									<input type="reset" name="button3" value="Vider le formulaire" style="width:130px;padding:7px;height:30px;background-color:AliceBlue;border-radius:20px">
-								</td>
-							</tr>
-					</table>
-				</form>
-			</body>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="tous.css">
+    <link rel="stylesheet" type="text/css" href="Vetements.css">
+</head>
+
+
+<body>
+    <!-- barre de navigation -->
+    <nav class="navbar navbar navbar-expand-lg navbar-lightgreen bg-lightgreen">
+
+  <a class="navbar-brand" href="Accueil.php"><img src="eceamazon.png" height="60px"></a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+&nbsp;&nbsp;&nbsp;&nbsp;
+
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+
+
+        <div class="dropdown">
+          <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Catégories
+          </a>
+
+
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="Livres.php" >Livres</a>
+                <a class="dropdown-item" href="Vetements.php">Vétements</a>
+                <a class="dropdown-item" href="Sport&Loisir.php">Sports et Loisir</a>
+                <a class="dropdown-item" href="Musique.php">Musique</a>
+               </div>
+        </div>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="Ventes_Flash.php">Ventes Flash</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Co_vendeur.php">Vendre</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Co_mon_compte.php">Mon Compte</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Co_admin.php">Admin</a>
+      </li>
+
+
+
+    </ul>
+  </div>
+
+  <a class="nav-link" href="Panier.php"><img src="panier.png"></a>
+
+</nav>
+
+<br>
+
+<br>
+
+
+<h2>Supprimer  musiques</h2>
+      <form action="Supprimer_musique.php" method="post">
+          <table>
+              <tr>
+                <td>Titre:</td>
+                <td><input type="text" name="titre"></td>
+              </tr>
+              <tr>
+                <td>Artiste:</td>
+                <td><input type="text" name="artiste"></td>
+              </tr>
+              <tr>
+                <td>Mail:</td>
+                <td><input type="text" name="mail"></td>
+              </tr>
+                <tr>
+                <td>
+                <input type="submit" name="button2" value="Supprimer"></td>
+
+                <td>
+                  <input type="reset" name="button3" value="Vider le formulaire">
+                </td>
+              </tr>
+          </table>
+        </form>
+
+
+
+
+
+
+
+
+ <footer class="footer">
+
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2019 Copyright:
+      <a href="Accueil.php"> ECE-Amazon.com</a>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
+
+
+
+  <!-- Footer -->
+<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
 </html>
