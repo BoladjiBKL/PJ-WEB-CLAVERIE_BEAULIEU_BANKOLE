92dfb50776e118c4d7fa7bbbb8367e5c55787ce8
MathieuClv <claveriemathieu@hotmail.fr>
2019-05-03 12:58:11 +0200

    PAiemeent

--- verif_co_compte_panier.php
+++ verif_co_compte_panier.php
@@ -77,6 +77,9 @@ if ($mdp =="") {
 
 if($donnees)
 {
+	session_start();
+/*session is started if you don't write this line can't use $_Session  global variable*/
+$_SESSION["newsession"]=$mailvendeur;
 	header('Location: Compte_client.php');
 }
 
